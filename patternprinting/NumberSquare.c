dd67ba0ea69f7f6d91c133c1b524d4e6b1893c25
Priyanshu Kumar Sharma <priyanshu.kumar.rcciit@gmail.com>
2023-11-03 23:39:28 +0530

    updateall

--- patternprinting/NumberSquare.c
+++ patternprinting/NumberSquare.c
@@ -4,16 +4,13 @@ int main()
     int n;
     printf("Enter a number:");
     scanf("%d",&n);
-    // for(int i=1;i<=n;i++)
-    // {
-    //     for(int i=1;i<=n;i++)
-    //     {
-    //         printf("1");
-    //     }
-    //     printf("\n");
-    // }
-    for(int i=0; i<n; i++){
-        newfile;
+    for(int i=1;i<=n;i++)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            printf("1");
+        }
+        printf("\n");
     }
     return 0;
 }
