b2978e10c76b71459ab4ec644eab13905003dbc2
Priyanshu Kumar Sharma <priyanshu.kumar.rcciit@gmail.com>
2024-04-09 20:19:44 +0530

    newcommit

--- patternprinting/BinaryTriangle.c
+++ patternprinting/BinaryTriangle.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+
+
 int main()
 {
     // "workbench.colorTheme": "Default Dark Modern",
@@ -7,7 +9,7 @@ int main()
     // "code-runner.clearPreviousOutput": true,
     // "code-runner.runInTerminal": true
     int n;
-    printf("Enter the number of rows:");
+    printf("Enter the number of rows : ");
     scanf("%d",&n);
     int a;
     for(int i=1;i<=n;i++)
@@ -16,7 +18,7 @@ int main()
         else a=0;
         for(int j=1;j<=i;j++)
         {
-            printf("%d",a);
+            printf("%d", a );
             if(a==0) a=1;
             else a=0;
         }
@@ -28,21 +30,3 @@ int main()
 
 
 
-#include<stdio.h>
-int main()
-{
-    int n;
-    printf("Enter the number of rows:");
-    scanf("%d",&n);
-    int a=1;
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=i;j++)
-        {
-            printf("%d",a);
-            a++;
-        }
-        printf("\n");
-    }
-    return 0;
-}
